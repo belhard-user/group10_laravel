6ff52e77c148c5a5c69c75aac6b7415d46d4a493
belhard-user <ldarik2l@gmail.com>
2017-01-21 12:32:07 +0300

    design

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,14 +6,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-
-    public function index($name = 'Guest')
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        return "welcome to our site $name !!!";
-    }
-
-    public function hello($name, $age)
-    {
-        return "hello $name i $age";
+        return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,5 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
-Route::get('welcome/{name?}', 'HomeController@index');
-
-Route::get('hello-{name}-{age}', 'HomeController@hello')->where([
-    'name' => '[a-z]+',
-    'age' => '\d{1,2}'
-]);
-
-
-Route::group(['prefix' => 'tag', 'namespace' => 'Article'], function(){
-    Route::get('article', ['uses' => 'ArticleController@index', 'as' => 'article']);
-});
-
-
-Route::group(['prefix' => 'secure'], function($r){
-    $r->get('dashboard', function(){
-        return "admin/dashbord";
-    });
-
-    $r->get('users', function(){
-        return "admin/users";
-    });
-
-    Route::get('article', function(){
-        return "admin/article";
-    });
-});
+Auth::routes();
